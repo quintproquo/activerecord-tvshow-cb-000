c99f0e0ff94d6cd8955d0d14ab90b7a831402580
Quinton Boltin <quinton@valleyexpetec.com>
2017-05-12 18:50:57 +0000

    Done.

---
+++ app/models/show.rb
@@ -0,0 +1,33 @@
+require 'pry'
+class Show < ActiveRecord::Base
+
+  def self.highest_rating
+    Show.maximum("rating")
+  end
+
+  def self.most_popular_show
+    Show.find_by_rating(Show.highest_rating)
+  end
+
+  def self.lowest_rating
+    Show.order(:rating).first.rating
+  end
+
+  def self.least_popular_show
+    Show.find_by_rating(Show.lowest_rating)
+  end
+
+  def self.ratings_sum
+    Show.sum(:rating)
+  end
+
+  def self.popular_shows
+    Show.where("rating > ?", 5)
+  end
+
+  def self.shows_by_alphabetical_order
+    binding.pry
+    Show.order(:name)
+  end
+
+end
